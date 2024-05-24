264b095def782cec793cfb22cf9a6584f607c69a
EloToJa <elotoja@protonmail.com>
2024-05-24 19:58:58 +0200

    Update Bomb.cs

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -20,7 +20,7 @@ public class Bomb : MonoBehaviour
         // stwórz eksplozjê, przeka¿ rozmiar od w³aœciciela bomby
 
         // zwiêksz w³aœcicielowi liczbê dostêpnych bomb
-
+        owner.IncreaseBombsRemaining();
         Destroy(gameObject);
     }
 }
